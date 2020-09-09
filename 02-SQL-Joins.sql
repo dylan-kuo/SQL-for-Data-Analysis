8bd6e0b3a94967593c207ac9729a0e1b2d04924a
Dylan Kuo <dylan.t.kuo@gmail.com>
2020-09-08 21:58:46 -0400

    add join

---
+++ 02-SQL-Joins.sql
@@ -0,0 +1,21 @@
+/*
+1. Try pulling all the data from the accounts table, and all the data from the 
+orders table.
+2. Try pulling standard_qty, gloss_qty, and poster_qty from the orders table, 
+and the website and the primary_poc from the accounts table.
+*/
+SELECT orders.*, accounts.*
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id
+
+SELECT orders.standard_qty, 
+       orders.gloss_qty, 
+	   accounts.website, 
+	   accounts.primary_poc
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id
+
+
+
