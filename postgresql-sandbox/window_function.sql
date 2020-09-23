513b0303f10fdef2f2d69b5f58529045f1767575
Dylan Kuo <dylan.t.kuo@gmail.com>
2020-09-23 00:15:03 -0400

    edit window functions

--- postgresql-sandbox/window_function.sql
+++ postgresql-sandbox/window_function.sql
@@ -125,6 +125,19 @@ SELECT
       product_name,	
 	  group_name,
 	  price,	  
+	  FIRST_VALUE(price) OVER (
+	      PARTITION BY group_name
+		  ORDER BY price
+	  ) AS lowest_price_per_group
+FROM products
+INNER JOIN product_groups USING (group_id)
+
+
+-- LAST_VALUE()
+SELECT 
+      product_name,	
+	  group_name,
+	  price,	  
 	  LAST_VALUE(price) OVER (
 	      PARTITION BY group_name
 		  ORDER BY price RANGE BETWEEN UNBOUNDED PRECEDING 
